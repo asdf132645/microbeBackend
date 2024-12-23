0ecdd4a51afcd333988df1842eb3f543b3d09839
henry <qooktree@gmail.com>
2024-12-23 14:51:21 +0900

    이미지 최적화 진행중

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -33,9 +33,11 @@ export class ImagesController {
 
     try {
       const imageBuffer = await sharp(absoluteImagePath)
-        .resize({ width: 290, height: 290, fit: 'contain' }) // 필요에 따라 크기 조정
-        .toFormat('webp')
-        .webp({ quality: 10 }) // WebP 포맷으로 설정
+        .toFormat('webp', {
+          quality: 10,
+          nearLossless: true
+        })
+        .resize({ width: 290, height: 290, fit: 'contain' })
         .toBuffer();
       // res.setHeader('Cache-Control', 'public, max-age=86400');
       res.setHeader('Content-Type', 'image/webp');
@@ -88,12 +90,16 @@ export class ImagesController {
     try {
       // 파일 접근 권한 확인
       fs.accessSync(absoluteImagePath, fs.constants.R_OK);
-
-      // 이미지 변환 및 응답 설정
-      const imageBuffer = fs.readFileSync(absoluteImagePath);
-      // res.setHeader('Cache-Control', 'public, max-age=86400');
-      res.contentType('image/bmp'); // BMP 이미지라면 Content-Type을 'image/bmp'로 설정
-      res.send(imageBuffer);
+      sharp(absoluteImagePath)
+        .resize({ width: 900 })
+        .toBuffer()
+        .then((imageBuffer) => {
+          res.contentType('image/webp');
+          res.send(imageBuffer);
+        })
+        .catch((error) => {
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Image processing error');
+        })
     } catch (error) {
       res
         .status(HttpStatus.NOT_FOUND)
@@ -127,6 +133,7 @@ export class ImagesController {
         .send('File not found or permission issue');
     }
   }
+
   @Get('getImageWbc')
   async getImageWbc(
     @Query('folder') folder: string,
@@ -134,155 +141,6 @@ export class ImagesController {
     @Res() res: Response,
   ) {
     await this.imagesService.getImageWbc(folder, imageName, res);
-  }
-
-  @Get('move')
-  async moveImage(
-    @Query('sourceFolders') sourceFolders: string,
-    @Query('destinationFolders') destinationFolders: string,
-    @Query('imageNames') imageNames: string,
-    @Res() res: Response,
-  ) {
-    const sourceFoldersArray = sourceFolders ? sourceFolders.split(',') : [];
-    const destinationFoldersArray = destinationFolders
-      ? destinationFolders.split(',')
-      : [];
-    console.log(imageNames);
-    const imageNamesArray = imageNames ? imageNames.split(',') : [];
-
-    // 매개변수 길이가 일치하는지 확인
-    if (
-      sourceFoldersArray.length !== destinationFoldersArray.length ||
-      sourceFoldersArray.length !== imageNamesArray.length
-    ) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ message: 'Invalid parameters' });
-    }
-
-    // 이미지 이동 처리 결과를 저장할 객체
-    const moveResults = {
-      success: [],
-      failed: [],
-    };
-    console.log(imageNamesArray);
-    for (let i = 0; i < imageNamesArray.length; i++) {
-      const imageName = imageNamesArray[i];
-      const absoluteSourcePath = path.join(sourceFoldersArray[i], imageName);
-      const absoluteDestinationPath = path.join(
-        destinationFoldersArray[i],
-        imageName,
-      );
-      console.log(absoluteSourcePath);
-      try {
-        // 파일 이동
-        fs.accessSync(absoluteSourcePath, fs.constants.R_OK);
-        fs.renameSync(absoluteSourcePath, absoluteDestinationPath);
-
-        // 성공 목록에 추가
-        moveResults.success.push(imageName);
-      } catch (error) {
-        // 실패 목록에 추가
-        moveResults.failed.push({ imageName, error: error.message });
-      }
-    }
-
-    // 이동 처리 결과를 응답으로 반환
-    return res.status(HttpStatus.OK).json(moveResults);
-  }
-
-  @Post('moveClassImage')
-  async moveClassImage(
-    @Body('sourceFolders') sourceFolders: any,
-    @Body('destinationFolders') destinationFolders: any,
-    @Body('fileNames') imageNames: any,
-    @Res() res: Response,
-  ) {
-    // 전달된 매개변수가 배열인지 확인하고 그대로 사용
-    const sourceFoldersArray = Array.isArray(sourceFolders)
-      ? sourceFolders
-      : [];
-    const destinationFoldersArray = Array.isArray(destinationFolders)
-      ? destinationFolders
-      : [];
-    const imageNamesArray = Array.isArray(imageNames) ? imageNames : [];
-
-    // 매개변수 길이 확인
-    if (
-      sourceFoldersArray.length !== destinationFoldersArray.length ||
-      sourceFoldersArray.length !== imageNamesArray.length
-    ) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ message: 'Invalid parameters' });
-    }
-
-    // 이미지 이동 처리 결과를 저장할 객체
-    const moveResults = {
-      success: [],
-      failed: [],
-    };
-
-    // 비동기 큐 설정
-    const concurrency = 10; // 동시에 처리할 파일 이동 작업 수
-    let activeTasks = 0;
-    const queue = [];
-
-    // 파일 이동 함수
-    const moveFile = async (
-      source: string,
-      destination: string,
-      imageName: string,
-    ) => {
-      try {
-        // 파일 접근 가능 확인
-        await fs.promises.access(source, fs.constants.R_OK);
-        // 파일 이동
-        await fs.promises.rename(source, destination);
-        // 성공 목록에 추가
-        moveResults.success.push(imageName);
-      } catch (error) {
-        // 실패 목록에 추가
-        moveResults.failed.push({ imageName, error: error.message });
-      } finally {
-        activeTasks--;
-        processQueue(); // 큐 처리 재개
-      }
-    };
-
-    // 큐 처리 함수
-    const processQueue = () => {
-      while (activeTasks < concurrency && queue.length > 0) {
-        const { source, destination, imageName } = queue.shift();
-        activeTasks++;
-        moveFile(source, destination, imageName);
-      }
-    };
-
-    // 큐에 작업 추가
-    for (let i = 0; i < sourceFoldersArray.length; i++) {
-      const source = path.join(sourceFoldersArray[i], imageNamesArray[i]);
-      const destination = path.join(
-        destinationFoldersArray[i],
-        imageNamesArray[i],
-      );
-      queue.push({ source, destination, imageName: imageNamesArray[i] });
-    }
-
-    // 큐 처리 시작
-    processQueue();
-
-    // 모든 작업이 완료될 때까지 기다림
-    await new Promise((resolve) => {
-      const checkCompletion = setInterval(() => {
-        if (activeTasks === 0 && queue.length === 0) {
-          clearInterval(checkCompletion);
-          resolve(null);
-        }
-      }, 100);
-    });
-
-    return res.status(HttpStatus.OK).json(moveResults);
   }
 
   @Post('upload')
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -12,42 +12,52 @@ export class ImagesService {
   constructor() {}
 
   async getImageWbc(folder: string, imageName: string, res: Response) {
-    if (!folder || !imageName) {
-      return res.status(HttpStatus.BAD_REQUEST).send('Invalid parameters');
-    }
+    if (!folder || !imageName) return res.status(HttpStatus.BAD_REQUEST).send('Invalid parameters');
 
-    // 이미지 경로를 받아와서 절대 경로로 조합
     const absoluteImagePath = path.join(folder, imageName);
 
     try {
-      fs.accessSync(absoluteImagePath, fs.constants.R_OK);
+      if (!fs.existsSync(absoluteImagePath)) {
+        console.error('File does not exist:', absoluteImagePath);
+        return res.status(HttpStatus.NOT_FOUND).send('File not found');
+      }
 
-      // bmp 이미지를 jpeg로 변환하여 압축
-      const jpegImageBuffer = await this.convertBmpToJpeg(absoluteImagePath);
+      const imageBuffer = await this.convertImageToWebP(absoluteImagePath);
 
-      // 압축된 이미지를 클라이언트에게 전송
-      res.setHeader('Content-Type', 'image/jpeg');
-      res.send(jpegImageBuffer);
+      res.setHeader('Content-Type', 'image/webp');
+      res.send(imageBuffer);
+
     } catch (error) {
-      res
-        .status(HttpStatus.NOT_FOUND)
-        .send('File not found or permission issue');
+      if (error.code === 'ENOENT') {
+        return res.status(HttpStatus.NOT_FOUND).send('File not found');
+      }
+
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Image processing failed');
     }
   }
 
-  // bmp 이미지를 jpeg로 변환하는 함수
-  private async convertBmpToJpeg(absoluteImagePath: string): Promise<Buffer> {
-    return new Promise((resolve, reject) => {
-      sharp(absoluteImagePath)
-        .jpeg({ quality: 80 }) // JPEG 압축 품질 조정
-        .toBuffer((err, buffer) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(buffer);
-          }
-        });
-    });
+  private convertImageToWebP = async (imagePath: string): Promise<Buffer> => {
+    try {
+      const image = await Jimp.read(imagePath);
+
+      // 이미지 너비 조정 (높이는 비율에 맞게 자동 조정)
+      const originalWidth = image.bitmap.width;
+      const originalHeight = image.bitmap.height;
+      const newWidth = 900;
+      const newHeight = Math.round((originalHeight / originalWidth) * newWidth);
+
+      // 이미지 리사이즈 및 WebP로 변환
+      const resizedImage = image
+        .resize(newWidth, newHeight)  // 너비 900으로 고정, 높이 비율 유지
+        .quality(60);  // 품질 설정 (0-100, 낮을수록 파일 크기 감소)
+
+      // WebP 버퍼로 변환
+      const pngBuffer = await resizedImage.getBufferAsync(Jimp.MIME_PNG);
+      return await sharp(pngBuffer).webp({ quality: 80}).toBuffer();
+    } catch (error) {
+      console.error('Image conversion error:', error);
+      throw new Error(`Failed to convert image: ${error.message}`);
+    }
   }
 
   async convertImageToJPEG(imagePath: string): Promise<Buffer> {
